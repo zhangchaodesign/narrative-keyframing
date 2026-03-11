e2604226fab5c6b17aedde6436240457c5240e91
Chao Zhang 张超 <zhangchaohci@gmail.com>
2026-03-11 19:29:56 -0400

    Add event trackers

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
